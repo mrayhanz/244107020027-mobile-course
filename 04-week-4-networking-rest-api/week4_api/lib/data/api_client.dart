1ebd47778b715001dfc9a70390ff9f5c458632f9
mrayhanz <rayymekdi@gmail.com>
2026-09-20 21:38:40 +0700

    praktikum-2

--- 04-week-4-networking-rest-api/week4_api/lib/data/api_client.dart
+++ 04-week-4-networking-rest-api/week4_api/lib/data/api_client.dart
@@ -3,10 +3,10 @@ import 'package:dio/dio.dart';
 Dio createDio() {
   final dio = Dio(
     BaseOptions(
-      baseUrl: 'https://jsonplaceholder.typicode.com',
+      baseUrl: 'https://jsonplaceholder1.typicode.com',
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
-      headers: {'Accept': 'application/json'},
+      headers: {'Accept': 'application/json'}, 
     ),
   );
   dio.interceptors.add(
